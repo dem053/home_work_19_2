e4d3285c53e9f014efd85c636c2ebb27007846b0
melde <mdv053@gmail.com>
2023-12-06 01:49:09 +0400

    Taask 4: add category.created_at

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,38 @@
+from datetime import date
+
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    created_at = models.DateField(verbose_name='дата создания', default=date.today())
+
+    def __str__(self):
+        return f'{self.category_name} - {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('category_name',)
+
+
+class Product(models.Model):
+    prod_name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание', default='описание отсутсвует')
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена')
+    date_created = models.DateField(verbose_name='дата создания', default=date.today)
+    date_change = models.DateField(verbose_name='дата изменения', auto_now=True)
+
+    def __str__(self):
+        return f'{self.prod_name} - {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('prod_name',)
+
