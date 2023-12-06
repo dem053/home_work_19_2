850ea0c654e8216d1bde86b4d22f57595996d0e5
melde <mdv053@gmail.com>
2023-12-06 22:15:15 +0400

    Taask 5: add admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name')
+    list_filter = ('category_name',)
+    search_fields = ('category_name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'prod_name', 'price', 'category')
+    list_filter = ('prod_name', 'category',)
+    search_fields = ('prod_name', 'category',)
